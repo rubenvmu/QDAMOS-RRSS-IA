015c4f4e17af4fea4ae254eca58cad6ddc88804d
Ruben Vasile Marcu Ungureanu <rvmu@rvmu.es>
2025-04-30 23:42:41 +0200

    0.0.7

---
+++ beMahou/Pages/Conciertos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespaces.Conciertos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ beMahou/Pages/Festivales/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace.Festivales
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
